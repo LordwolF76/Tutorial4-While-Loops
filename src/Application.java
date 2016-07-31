6578518d2ca821751af9837139c9e5fafc358b3f
LordwolF76 <lordwolf76@gmail.com>
2016-07-31 16:07:22 -0700

    Initial commit

---
+++ src/Application.java
@@ -0,0 +1,15 @@
+/**
+ * Created by LordwolF on 7/31/2016.
+ */
+public class Application {
+    public static void main(String[] args) {
+        int value = 10;
+
+        while(value < 20)
+        {
+            System.out.println("Running " + value);
+
+            value = value + 1;
+        }
+    }
+}
